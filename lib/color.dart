8b89a30e57581b7e954500aed909ecf6726921d5
alizeroukiaya0911 <alizeroukiaya0911@gamil.com>
2023-06-06 23:41:42 +0200

    project

---
+++ lib/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = Color.fromARGB(255, 83, 214, 210);
+  static Color secondaryColor = Color.fromARGB(255, 245, 250, 169);
+  static Color accentColor = Color.fromARGB(40, 65, 104, 232);
+}
